d299f4aadcc716d7cc6eab86e64330c261f356f9
Peter Johnstone <peter.wdj@gmail.com>
2017-10-08 16:53:20 +0100

    Changed each method within print method to while loop (control flow)

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-#first we add all the studrnts to an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #empty array into which we will save the newly inputted students
+  students = []
+  #getting the first name to add
+  name = gets.chomp
+  #code to repeat whilst the name is not empty
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -19,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  counter = 0
+  while counter < students.length do
+    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]})"
+    counter += 1
   end
 end
 
@@ -48,27 +54,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #empty array into which we will save the newly inputted students
-  students = []
-  #getting the first name to add
-  name = gets.chomp
-  #code to repeat whilst the name is not empty
-  while !name.empty? do
-    #add the student has to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end
-  #return the array of students
-  students
-end
-
 #nothing happens until we call the methods
 students = input_students
 print_header
-print_under_12(students)
+print(students)
 print_footer(students)
