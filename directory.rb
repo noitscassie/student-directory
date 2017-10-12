52bfbd21458b0b2b28526fba5bb7d55458b74843
Peter Johnstone <peter.wdj@gmail.com>
2017-10-12 13:34:08 +0100

    Added checker to see if the student is in the directory

--- directory.rb
+++ directory.rb
@@ -172,6 +172,14 @@ def remove_student
   print_by_cohort
   puts "Which student would you like to remove? Please type in that student's name"
   student_to_remove = STDIN.gets.chomp
+  loop do
+    if !(@students.any? { |student| student[:name] == student_to_remove})
+      puts "Sorry, #{student_to_remove} is not in the directory. Please re-enter the name of the student you would like to remove"
+    else
+      break
+    end
+    student_to_remove = STDIN.gets.chomp
+  end
   @students.delete_if {|student| student.fetch(:name) == student_to_remove}
   puts "#{student_to_remove} has been deleted from the directory!"
 end
